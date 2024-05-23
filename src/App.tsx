fcf6e165b7eb80dce62cb60650838c790214b274
Will Ness <willness210@gmail.com>
2024-05-23 13:15:32 -0600

    set up package

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.css";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <p className="text-yellow-500">
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+    </div>
+  );
+};
+
+export default App;
